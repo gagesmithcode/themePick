ab135cd964d0707ca98caeac23fe9bc3a26dd919
Gage Smith <gage.smith.andrew@gmail.com>
2020-01-30 19:48:34 -0800

    Adding the code for the popup and the functionality as well as the images associated with the popup itself

---
+++ themePick/popup.js
@@ -0,0 +1,27 @@
+/*
+ * Project       : ThemePick Extension
+ * FileName      : popup.js
+ * Author        : Gage Smith
+ * Description   :
+ *
+*/
+
+//this grabs the button from popup.html and requests the color value from storage
+//Then it applies that color as the backround of the button
+let changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
+
+//This should add logic for further user interaction
+//This is an "onclick" event for the changecolor functionality of the button
+changeColor.onclick = function(element) {
+  let color = element.target.value;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  });
+};
