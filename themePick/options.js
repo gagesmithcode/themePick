7a8e0967c0bad3786c8023a0102db6e3bb19399e
Gage Smith <gage.smith.andrew@gmail.com>
2020-01-30 19:41:52 -0800

    Added an options page and the functionality for it to allow the user to select the color that will appear on the popup

---
+++ themePick/options.js
@@ -0,0 +1,26 @@
+/*
+ * Project       : ThemePick Extension
+ * FileName      : options.js
+ * Author        : Gage Smith
+ * Description   :
+ *
+*/
+//this portion adds logic to the options html file
+let page = document.getElementById('buttonDiv');
+
+//This is a const object of the four color options you can chose from on the page as background colors
+const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+function constructOptions(kButtonColors) {
+  for (let item of kButtonColors) {
+    let button = document.createElement('button');
+    button.style.backgroundColor = item;
+    button.addEventListener('click', function() {
+      chrome.storage.sync.set({color: item}, function() {
+      console.log('color is' + item);
+      })
+    });
+    page.appendChild(button);
+  }
+}
+
+constructOptions(kButtonColors);
