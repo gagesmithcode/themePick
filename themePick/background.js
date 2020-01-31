f9836e88da046115cc30eac8d02613565201cdcf
Gage Smith <gage.smith.andrew@gmail.com>
2020-01-30 19:40:58 -0800

    Added all permissions, an options page and icons to the manifest as well as changed the background functionality to allow for users to interact with the popup upon clicking the extension

--- themePick/background.js
+++ themePick/background.js
@@ -2,7 +2,7 @@
  * Project       : ThemePick Extension
  * FileName      : background.js
  * Author        : Gage Smith
- * Description   : 
+ * Description   :
  *
 */
 
@@ -10,4 +10,15 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#666666'}, function() {
     console.log("The color is grey.");
   });
+
+//This portion will create the functionallity difference where the user can actually interact with popup.html
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'developer.chrome.com'},
+        })
+      ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
 });
