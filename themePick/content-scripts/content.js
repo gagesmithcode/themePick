bfa334afc92c2279eab77a3974a4c3fa6eaa5f17
Gage Smith <gage.smith.andrew@gmail.com>
2020-04-09 09:37:10 -0700

    Added some basic websites that are attached to the extension.

--- themePick/content-scripts/content.js
+++ themePick/content-scripts/content.js
@@ -6,3 +6,14 @@
  *
 */
 // alert("ohhh so much dark")
+
+// chrome.runtime.onMessage.addListener(gotMessage);
+// function gotMessage(message, sender, sendresponse)
+// {
+//     console.log(message.txt);
+//     let para = document.getElementsByTagName("p");
+//     for(elt of para)
+//     {
+//         elt.style['background-color'] = '#212121';
+//     }
+// }
