6a81f089a067c856c745e695d3e8ceb0db9e0713
Gage Smith <gage.smith.andrew@gmail.com>
2020-04-27 09:30:03 -0700

    Working around with a different was to have an onclick event of changing the webpages theme.

--- themePick/background/background.js
+++ themePick/background/background.js
@@ -4,21 +4,23 @@
  * Author        : Gage Smith
  * Description   :
  *
-*/
+ */
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#666666'}, function() {
-    console.log("The color is grey.");
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({ color: "#212121" }, function () {
   });
 
-//This portion will create the functionallity difference where the user can actually interact with popup.html
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          // pageUrl: {hostEquals: 'developer.chrome.com'}, //This portion says what hosts should be effected by the extension. Need to get to work for all hosts
-        })
-      ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
+  //This portion will create the functionallity difference where the user can actually interact with popup.html
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            // pageUrl: {hostEquals: 'developer.chrome.com'}, //This portion says what hosts should be effected by the extension. Need to get to work for all hosts
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ]);
+  });
 });
