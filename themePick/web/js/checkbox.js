e403f8cb96b92ff044a0a19b4c00094e6ac4bb46
Gage Smith <gage.smith.andrew@gmail.com>
2020-05-06 10:36:47 -0700

    Changed toggle document query grab to grab the new checkbox id 'blackboard'.

--- themePick/web/js/checkbox.js
+++ themePick/web/js/checkbox.js
@@ -11,7 +11,8 @@ App.prototype.getState = function () {
 function init() {
   var app = new App();
   var state = app.getState();
-  var checkbox = document.querySelector("#themeOn");
+  //TODO: create a checkbox specifically for other sites 
+  var checkbox = document.querySelector("#blackboard");
 
   if (state == "true") {
     checkbox.checked = true;
@@ -23,6 +24,7 @@ function init() {
       app.setState(checkbox.checked);
       chrome.tabs.reload();
     });
+    //Set up the port to communicate with background.js
     var port = chrome.runtime.connect({ name: "porty" });
     if (checkbox.checked == true) port.postMessage({ state: "enabled" });
     else if (checkbox.checked == false) port.postMessage({ state: "disabled" });
