96ace81d6a348d977d3e25cc263b69ab7bf01699
Gage Smith <gage.smith.andrew@gmail.com>
2020-05-06 10:38:14 -0700

    Added a tabQuery function to reduce code redundancy. Also changed the url check to make see if the pendingUrl is within constraints allowing for theme to be onloaded when heading to the site. This makes reloading the site for theme application not an issue.

--- themePick/background/background.js
+++ themePick/background/background.js
@@ -10,6 +10,7 @@ var tabUrl;
 var tabId;
 
 var colorFlag = true;
+//Listening on port to determine whether the theme is toggled on or off
 chrome.runtime.onConnect.addListener(function (port) {
   console.assert(port.name == "porty");
   port.onMessage.addListener(function (msg) {
@@ -21,29 +22,23 @@ chrome.runtime.onConnect.addListener(function (port) {
 chrome.tabs.onCreated.addListener(changeColor);
 
 //Enabling web navigation to have the changeColor function fire before loading of the webpage
-chrome.webNavigation.onBeforeNavigate.addListener(function ({ tabId: tabId }) {
-  chrome.tabs.query(
-    { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
-    function (curTab) {
-      if (curTab[0].url && curTab[0].url.indexOf("csuchico.edu") != -1) {
-        tabUrl = curTab[0].url;
-        tabId = curTab[0].id;
-      }
-    }
-  );
+chrome.webNavigation.onBeforeNavigate.addListener(function () {
+  tabQuery();
   changeColor();
 });
 
 chrome.webNavigation.onCommitted.addListener(function () {
+  tabQuery();
   changeColor();
 });
 //Take the incoming tab id and check if it is a Blackboard page
 //If blackboard then layer the css over it
 function changeColor() {
-  // var tabUrl = tab.url;
   if (colorFlag) {
-    // if (tabUrl && tabUrl.indexOf("learn.csuchico.edu") != -1) {
-    if (tabUrl && tabUrl.indexOf("csuchico.edu") != -1) {
+    if (
+      (tabUrl && tabUrl.indexOf("learn.csuchico.edu") != -1) ||
+      (tabUrl && tabUrl.indexOf("shibboleth.csuchico.edu") != -1)
+    ) {
       // window.alert("BEFORE");
       chrome.tabs.insertCSS(tabId, {
         file: "styles/blackboard.css",
@@ -52,4 +47,23 @@ function changeColor() {
       });
     }
   }
+}
+
+//Function to get the current active Url and tab ID
+//TODO: make available for only the sites list dynamically
+function tabQuery() {
+  chrome.tabs.query(
+    { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
+    function (curTab) {
+      if (
+        (curTab[0].pendingUrl &&
+          curTab[0].pendingUrl.indexOf("csuchico.edu") != -1) ||
+        (curTab[0].pendingUrl &&
+          curTab[0].pendingUrl.indexOf("shibboleth.csuchico.edu") != -1)
+      ) {
+        tabUrl = curTab[0].pendingUrl;
+        tabId = curTab[0].id;
+      }
+    }
+  );
 }
